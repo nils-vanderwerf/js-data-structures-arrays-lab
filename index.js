f47ebaea22db74880ce6223cc7a7633d50971fdf
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-10 15:29:48 +1100

    Passing all tests

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+// Write your solution here
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(params) {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift(name);
+}
+
+function appendDriver(name) {
+    let newArray = drivers.concat(name);
+    return newArray;
+
+}
+
+function prependDriver(name) {
+    //Using spread
+    let newArray2 = [name, ...drivers];
+    return newArray2;
+}
+
+function removeLastDriver() {
+  let newArr = drivers.slice(0, -1);
+  return newArr;
+}
+
+
+function removeFirstDriver() {
+    let newArr = drivers.slice(1, 4);
+    console.log(newArr);
+    return newArr;
+  }
